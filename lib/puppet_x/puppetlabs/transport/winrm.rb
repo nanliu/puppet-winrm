ed60eade1119d83d8837acf0f81d76f188911b7d
Nan Liu <nan.liu@gmail.com>
2014-03-06 21:31:25 +0000

    Add winrm powershell provider to exec type.

--- lib/puppet_x/puppetlabs/transport/winrm.rb
+++ lib/puppet_x/puppetlabs/transport/winrm.rb
@@ -10,23 +10,22 @@ module PuppetX::Puppetlabs::Transport
       options = opts[:options] || {}
       @options = options.inject({}){|h, (k, v)| h[k.to_sym] = v; h}
 
-      @port = @options.fetch(:port, 5985)
+      port = @options.fetch(:port, 5985)
       @connection = @options.fetch(:connection, :plaintext)
-      case connection
+      case @connection
       when :plaintext
-        @endpoint = "http://#{opts[:server]}:#{opts[:port]}/wsman"
+        @endpoint = "http://#{opts[:server]}:#{port}/wsman"
         @options[:user] = opts[:username]
         @options[:pass] = opts[:password]
-        @options[:basic_auth_only] ||= true
-        @options[:disable_sspi] ||= true
+        #@options[:basic_auth_only] ||= true
+        @options[:disable_sspi] ||= true unless @options[:basic_auth_only]
       when :ssl
-        @endpoint = "https://#{opts[:server]}:#{opts[:port]}/wsman"
+        @endpoint = "https://#{opts[:server]}:#{port}/wsman"
         @options[:user] = opts[:username]
         @options[:pass] = opts[:password]
-        @options[:basic_auth_only] ||= true
-        @options[:disable_sspi] ||= true
+        @options[:disable_sspi] ||= true unless @options[:basic_auth_only]
       when :kerberos
-        @endpoint = "https://#{opts[:server]}:#{opts[:port]}/wsman"
+        @endpoint = "https://#{opts[:server]}:#{port}/wsman"
       end
       Puppet.debug("#{self.class} initializing connection to: #{@options[:host]}")
     end
